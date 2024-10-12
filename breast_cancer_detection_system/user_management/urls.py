d8a2b33c525bcd938f23740048697a0e9204ebbc
Ricardo Santos <ricardosantos130100@gmail.com>
2024-10-12 18:06:46 -0300

    Inicio de funções na tela inicial e armazenamento de imagens em Bucket no s3
    Signed-off-by: Ricardo Santos <ricardosantos130100@gmail.com>

--- breast_cancer_detection_system/user_management/urls.py
+++ breast_cancer_detection_system/user_management/urls.py
@@ -10,14 +10,36 @@ app_name = "user_management"
 urlpatterns = [
     path("register/", views.register, name="register_user"),
     path("login/", views.login, name="login"),
-    path("password_reset/", auth_views.PasswordResetView.as_view(template_name="user_management/password_reset_form.html",
-                                email_template_name="user_management/password_reset_email.html",
-                                 success_url=reverse_lazy('user_management:password_reset_done')), name="password_reset"),
-path("password_reset/done/", auth_views.PasswordResetDoneView.as_view(template_name="user_management/password_reset_done.html"),
-name="password_reset_done"),
-
-    path("password_reset/<uidb64>/<token>/", auth_views.PasswordResetConfirmView.as_view(template_name="user_management/password_reset_confirm.html"),
-    name="password_reset_confirm"),
-    path("reset/done/", auth_views.PasswordResetCompleteView.as_view(template_name="user_management/password_reset_complete.html"),
-    name="password_reset_complete"),
+    path(
+        "password_reset/",
+        auth_views.PasswordResetView.as_view(
+            template_name="user_management/password_reset_form.html",
+            email_template_name="user_management/password_reset_email.html",
+            success_url=reverse_lazy("user_management:password_reset_done"),
+            # success_url = "user_management:password_reset_done"
+        ),
+        name="password_reset",
+    ),
+    path(
+        "password_reset/done/",
+        auth_views.PasswordResetDoneView.as_view(
+            template_name="user_management/password_reset_done.html"
+        ),
+        name="password_reset_done",
+    ),
+    path(
+        "password_reset/<uidb64>/<token>/",
+        auth_views.PasswordResetConfirmView.as_view(
+            template_name="user_management/password_reset_confirm.html",
+            success_url = "user_management:password_reset_complete"
+        ),
+        name="password_reset_confirm",
+    ),
+    path(
+        "reset/done/",
+        auth_views.PasswordResetCompleteView.as_view(
+            template_name="user_management/password_reset_complete.html"
+        ),
+        name="password_reset_complete",
+    ),
 ]
